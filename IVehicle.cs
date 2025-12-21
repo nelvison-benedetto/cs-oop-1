a8df73518bac3eb18c65593bf1e50dcfc273e904
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2025-12-21 19:08:38 +0100

    update

--- IVehicle.cs
+++ IVehicle.cs
@@ -14,9 +14,9 @@ namespace cs_oop_1
         public void stop() {   //'default', ma se scrivi esplicitamente 'default' da error, cmnq ok , ovveride opzionale
             Console.WriteLine("Vehicle stopping...");
         }
-        public static void printInfoInterface()   //static
+        public static void printInfoInterface()   //static, chiamalo direttamente w IVehicle.printInfoInterface();
         {
-            Console.WriteLine("This is Vehicle interface.");  //call with IVehicle.printInfoInterface();
+            Console.WriteLine("This is Vehicle interface.");
         }
         private void myPrivateMethod(string mex)  //private
         {
